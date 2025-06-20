5d886e4d1b2e7d39ee5221fe9f412e2349824e51
meera0305 <chavanmeera66@gmail.com>
2025-06-20 11:35:15 +0530

    Create book.java

---
+++ book.java
@@ -0,0 +1,12 @@
+// File: Book.java
+
+public class Book {
+    int id;
+    String title, author;
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
