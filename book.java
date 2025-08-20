f8504c515f8f7f76db0803d450cbfecf1d1cae0d
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-08-20 10:30:52 +0530

    Update book.java

--- book.java
+++ book.java
@@ -6,7 +6,7 @@ public class Book {
 
     public Book(int id, String title, String author) {
         this.id = id;
-        this.title = title;
+        this.title = title;  
         this.author = author;
     }
 }
