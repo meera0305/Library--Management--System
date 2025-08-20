0d9ffcf6527541651fe626d48b9ab2e3b3772df1
Meera Santram Chavan <chavanmeera66@gmail.com>
2025-08-20 10:31:10 +0530

    Update libraryMain.java

--- libraryMain.java
+++ libraryMain.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class LibraryMain {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         int choice;
 
         do {
